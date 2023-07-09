01e2bb3e38c95b2e43f8ee1f4fbd2b44a41803f4
top_acdemy_student <top_acdemy_student@OR-08>
2023-07-09 13:18:10 +0700

    Добавьте файлы проекта.

---
+++ GarbunCollector!/ListWork.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbunCollector_
+{
+    internal class ListWork:IDisposable
+    {
+        private const char Separator = '\n';
+
+        public List<string> ToDos { get; set; }
+
+        public ListWork()
+        {
+            if (File.Exists("todos.txt")==true)
+            {
+                FileStream fs = new FileStream("todos.txt",FileMode.Open);
+            }
+            else
+            {
+               File.Create("todos.txt");
+               FileStream fs = new FileStream("todos.txt", FileMode.Open);
+               byte[] buffer = new byte[1024];
+               fs.Read(buffer, 0, buffer.Length);
+               string st = buffer.ToString();
+               fs.Close();
+               
+                ToDos = st.Split(Convert.ToChar(Separator)).ToList();
+               
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GarbunCollector!/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using Microsoft.Win32;
+class ShowRegistry
+{
+    public static void WriteInfo()
+    {
+        // запись информации о запуске в реестр
+        RegistryKey currentUser = Registry.CurrentUser;
+        using (RegistryKey info = currentUser.CreateSubKey("INFO"))
+        {
+            info.SetValue("RUN", 1);
+            if (info.GetValue("Count") == null)
+                info.SetValue("Count", 0);
+            else
+            {
+                int count = (int)info.GetValue("Count");
+                count++;
+                info.SetValue("Count", count);
+            }
+        }
+    }
+
+    public static bool CheckInfo()
+    {
+        // проверить, установлен ли параметр User/INFO/RUN
+        RegistryKey currentUser = Registry.CurrentUser;
+        using (RegistryKey info = currentUser.OpenSubKey("INFO"))
+        {
+            if (info == null)
+            {
+                return false;
+            }
+            object value = info.GetValue("RUN");
+            return value != null;
+        }
+    }
+
+    public static void Main()
+    {
+        // Задача: написать программу, которая должна знать, запускалась
+        // ли она ранее на этом компьютере
+        // Использовать реестр
+        if (CheckInfo())
+        {
+            WriteInfo();
+            RegistryKey cur = Registry.CurrentUser;
+            using (RegistryKey info = cur.OpenSubKey("INFO"))
+            {
+               
+                Console.WriteLine($"Программа запускалась ранее на этом устройстве {info.GetValue("Count")}");
+            }
+            
+
+        }
+        else
+        {
+            WriteInfo();
+            Console.WriteLine("Программа запущена первый раз");
+        }
+        Console.ReadLine();
+    }
+}
